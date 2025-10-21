230d346efad5522811f983cbc826723719d1637a
Ryan Luong <lumintriet@gmail.com>
2025-10-21 10:53:14 +0700

    chore(ui): add button and dropdown menu components from shadcn/ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <h1>Dev Store</h1>;
+  return (
+    <div className="flex flex-col items-center justify-center h-screen w-screen gap-y-4">
+      <h1 className="text-2xl font-bold">Dev Store</h1>
+      <Button>Button</Button>
+    </div>
+  );
 }
