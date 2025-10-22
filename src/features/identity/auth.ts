cb53c0992ad7fe549edbfba727e43e8f18dc3bd6
Ryan Luong <lumintriet@gmail.com>
2025-10-22 09:36:38 +0700

    chore(auth): connect drizzle with auth

--- src/features/identity/auth.ts
+++ src/features/identity/auth.ts
@@ -1,9 +1,14 @@
 import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { db } from "@/db/connection";
 import { serverEnvVars } from "@/env/server";
 
 const auth = betterAuth({
   secret: serverEnvVars.AUTH_SECRET,
   baseURL: serverEnvVars.AUTH_URL,
+  database: drizzleAdapter(db, {
+    provider: "pg",
+  }),
 });
 
 export { auth };
