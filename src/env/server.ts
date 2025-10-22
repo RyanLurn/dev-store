f53da646b77a7fcad45cd622f89520b03d6e4cd1
Ryan Luong <lumintriet@gmail.com>
2025-10-22 09:35:11 +0700

    chore(auth): configure an auth instance with env vars

--- src/env/server.ts
+++ src/env/server.ts
@@ -5,6 +5,8 @@ const serverEnvVars = createEnv({
   server: {
     DATABASE_DIRECT_CONNECTION_STRING: z.string().min(1),
     DATABASE_POOLED_CONNECTION_STRING: z.string().min(1),
+    AUTH_SECRET: z.string().min(1),
+    AUTH_URL: z.url(),
   },
   experimental__runtimeEnv: process.env,
 });
